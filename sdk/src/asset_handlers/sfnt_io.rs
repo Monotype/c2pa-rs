73990217f7e8e3ed62180d093692fadc3b90d84a
Christopher Kush (Monotype) <christopher.kush@monotype.com>
2023-12-12 13:18:07 -0700

    Copyright.

--- sdk/src/asset_handlers/sfnt_io.rs
+++ sdk/src/asset_handlers/sfnt_io.rs
@@ -1,4 +1,4 @@
-// Copyright 2023 Monotype. All rights reserved.
+// Copyright 2022,2023 Monotype. All rights reserved.
 // This file is licensed to you under the Apache License,
 // Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
 // or the MIT license (http://opensource.org/licenses/MIT),
